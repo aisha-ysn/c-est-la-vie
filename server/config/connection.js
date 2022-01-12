797976097cee1454f9840e12919cf7bf15da1af7
Aisha <aishayasin1801@gmail.com>
2022-01-12 20:25:13 +0000

    chore: connection update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/journals", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/c-est-la-vie", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
