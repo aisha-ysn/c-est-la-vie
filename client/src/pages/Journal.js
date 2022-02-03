9b74ff38ba412336cda30558bd0c35bc012cdc5d
FJVIKTORIJA <83015268+FJVIKTORIJA@users.noreply.github.com>
2022-02-03 22:12:49 +0000

    Refracted code
    Refactored code in  journal.js

--- client/src/pages/Journal.js
+++ client/src/pages/Journal.js
@@ -1,62 +1,63 @@
-import React, { Component } from 'react';
-import '../assets/css/journal.css';
+import React, { useState } from "react";
+import "../assets/css/journal.css";
+import { useMutation, useQuery } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import { SAVE_ENTRIES } from "../utils/mutations";
+import Auth from "../utils/auth";
 
-class Form extends Component {
-	constructor(props) {
-		super(props);
+const Form = () => {
+  const { loading, data } = useQuery(GET_ME);
+  const [saveEntry, { error }] = useMutation(SAVE_ENTRIES);
+  const [entry, setEntry] = useState({
+    title: "",
+    content: "",
+  });
+  const userData = data?.me || {};
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setEntry({
+      ...entry,
+      [name]: value,
+    });
+  };
+  const handleEntrySave = async () => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {
+      return false;
+    }
+    try {
+      const { data } = await saveEntry({
+        variables: { input: { ...entry } },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div>
+      <form>
+        <p className="line-1 anim-typewriter"> C&apos;est La Vie </p>
+        <input
+          placeholder="Date"
+          type="text"
+          name="title"
+          id="title"
+          onChange={handleChange}
+        />
 
-		this.initialState = {
-			title: '',
-			body: ''
-		};
-
-		this.state = this.initialState;
-	}
-
-	handleChange = (event) => {
-		const { name, value } = event.target;
-
-		this.setState({
-			[name]: value
-		});
-	};
-
-	submitForm = () => {
-		this.props.handleSubmit(this.state);
-		this.setState(this.initialState);
-	};
-
-	render() {
-		const { title, body } = this.state;
-
-		return (
-			<div>
-				<form>
-					<p className="line-1 anim-typewriter"> C&apos;est La Vie </p>
-					<input
-						placeholder="Date"
-						type="text"
-						name="title"
-						id="title"
-						value={title}
-						onChange={this.handleChange}
-					/>
-
-					<textarea
-						placeholder="Journal Text..."
-						type="textarea"
-						name="body"
-						id="body"
-						value={body}
-						onChange={this.handleChange}
-					/>
-				</form>
-				<button onClick={this.submitForm} className="button">
-					Log Journal
-				</button>
-			</div>
-		);
-	}
-}
+        <textarea
+          placeholder="Journal Text..."
+          type="textarea"
+          name="content"
+          id="body"
+          onChange={handleChange}
+        />
+      </form>
+      <button onClick={handleEntrySave} className="button">
+        Log Journal
+      </button>
+    </div>
+  );
+};
 
 export default Form;
